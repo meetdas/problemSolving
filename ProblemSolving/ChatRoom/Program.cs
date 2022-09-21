450b83bea61edfaded317c829f252f993f0a952b
devskil <meet.dasnato@gmail.com>
2022-09-21 17:14:08 +0600

    ChatRoom problem is solved.

---
+++ ProblemSolving/ChatRoom/Program.cs
@@ -0,0 +1,22 @@
+﻿string desired = "hello";
+string userInput = Console.ReadLine();
+string builtWord = "";
+int index = 0;
+for (int I = 0; I < userInput.Length; I++)
+{
+    if (userInput[I] == desired[index])
+    {
+        builtWord= builtWord + (userInput[I]);
+        index++;
+        if (index >= desired.Length)
+            break;
+    }
+}
+if (builtWord.ToLower() == desired)
+{
+    Console.WriteLine("YES");
+}
+else
+    Console.WriteLine("NO");
+
+
---
+++ ProblemSolving/HQ9+ProgrammingLanguage/Program.cs
@@ -0,0 +1,20 @@
+﻿string givenString = Console.ReadLine();
+int positiveResultCount = 0;
+for (int i = 0; i < givenString.Length; i++)
+{
+    if (givenString[i] == 'H' || givenString[i] == 'Q' || givenString[i] == '9')
+        positiveResultCount++;
+    if (positiveResultCount > 0)
+        break;
+}
+if (positiveResultCount > 0)
+    {
+        Console.WriteLine("YES");
+    }
+else
+    {
+        Console.WriteLine("NO");
+    }
+
+
+
